8c75b999ac45cdb6218715812f20ac0831bdc540
GeorgePrime <georgev45@hotmail.com>
2021-12-07 00:24:33 -0800

    Makes predictions with the trained neural network

---
+++ predictions.py
@@ -0,0 +1,42 @@
+from keras.models import model_from_json
+from pathlib import Path
+from keras.preprocessing import image
+import numpy as np
+
+# These are the class labels from the training data (in order from 0 to 9)
+class_labels = ["cane", "cavallo", "elefante", "farfalla", "gallina", "gatto", "mucca", "pecora", "ragno", "scoiattolo"]
+
+# Load the json file that contains the model's structure
+f = Path("model_structure.json")
+model_structure = f.read_text()
+
+# Recreate the Keras model object from the json data
+model = model_from_json(model_structure)
+
+# Re-load the model's trained weights
+model.load_weights("model_weights.h5")
+
+# Load an image file to test, resizing it to 128x128 pixels (as required by this model)
+img = image.load_img("greycat.jpg", target_size=(128, 128))
+
+# Convert the image to a numpy array
+image_to_test = image.img_to_array(img) / 255
+
+# Add a fourth dimension to the image (since Keras expects a list of images, not a single image)
+list_of_images = np.expand_dims(image_to_test, axis=0)
+
+# Make a prediction using the model
+results = model.predict(list_of_images)
+
+# Since we are only testing one image, we only need to check the first result
+single_result = results[0]
+
+# We will get a likelyhood score of all 10 possible classes. Find out which class had the highest
+most_likely_class_index = int(np.argmax(single_result))
+class_likelihood = single_result[most_likely_class_index]
+
+# Get the name of the most likely class
+class_label = class_labels[most_likely_class_index]
+
+# Print the result
+print("This is image is a {} - Likelihood: {:2f}".format(class_label, class_likelihood))
